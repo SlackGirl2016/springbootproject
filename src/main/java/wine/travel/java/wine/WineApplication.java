c4a245a89febe2ace866897bf4bacf49c5773f79
Naty Cuaquehua Amador <naty_1992_itsz@hotmail.com>
2020-09-30 13:29:32 -0400

    Working with ribbon

--- src/main/java/wine/travel/java/wine/WineApplication.java
+++ src/main/java/wine/travel/java/wine/WineApplication.java
@@ -2,14 +2,41 @@ package wine.travel.java.wine;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import wine.travel.java.wine.config.RibbonConfiguration;
 
 @SpringBootApplication
+@RestController
+@RibbonClient(
+  name = "ping-a-server",
+  configuration = RibbonConfiguration.class)
 public class WineApplication {
+	
+	@LoadBalanced
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+ 
+    @Autowired
+    RestTemplate restTemplate;
+ 
+    @RequestMapping("/server-location")
+    public String serverLocation() {
+        return this.restTemplate.getForObject(
+          "http://ping-server/locaus", String.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineApplication.class, args);
