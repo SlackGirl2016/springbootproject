05242f0d943d6ff52fd35b72a2e8046299d7503d
Naty Cuaquehua Amador <naty_1992_itsz@hotmail.com>
2020-09-30 12:37:03 -0400

    My firts commit

---
+++ src/main/java/wine/travel/java/wine/ServletInitializer.java
@@ -0,0 +1,13 @@
+package wine.travel.java.wine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WineApplication.class);
+	}
+
+}
